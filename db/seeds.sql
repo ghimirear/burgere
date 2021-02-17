532d7bf51876549d05267c51f8cec7150e235986
Arjun Ghimire <ghimirearjun15@gmail.com>
2021-02-17 13:33:09 -0500

    all the frameworks schema seeds and mvc setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken', false);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('beyond', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('beef', false);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,105 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+      console.info('DOM loaded');
+    }
+  
+    // UPDATE
+    const changeDevouredState = document.querySelectorAll('.devour-burger');
+  
+    // Set up the event listener for the create button
+    if (changeDevouredState) {
+        changeDevouredState.forEach((button) => {
+        button.addEventListener('click', (e) => {
+          // Grabs the id of the element that goes by the name, "id"
+          const id = e.target.getAttribute('data-id');
+          
+  
+          const toDevour = {
+            devoured : true,
+          };
+  
+          fetch(`/api/burgers/${id}`, {
+            method: 'PUT',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+  
+            // make sure to serialize the JSON body
+            body: JSON.stringify(toDevour),
+          }).then((response) => {
+            // Check that the response is all good
+            // Reload the page so the user can see the new quote
+            if (response.ok) {
+              console.log(`changed sleep to: devoured`);
+              location.reload('/');
+            } else {
+              alert('something went wrong!');
+            }
+          });
+        });
+      });
+    }
+  
+    // CREATE
+    const createBurgerBtn = document.getElementById('create-form');
+  
+    if (createBurgerBtn) {
+      createBurgerBtn.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if(document.getElementById('burger').value.trim() === ''){
+            return;
+        };
+        // Grabs the value of the textarea that goes by the name, "quote"
+        const newBurger = {
+          name: document.getElementById('burger').value.trim(),
+          devoured : false,
+        };
+  
+        // Send POST request to create a new quote
+        fetch('/api/burgers', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+  
+          // make sure to serialize the JSON body
+          body: JSON.stringify(newBurger),
+        }).then(() => {
+          // Empty the form
+          document.getElementById('burger').value = '';
+  
+          // Reload the page so the user can see the new quote
+          console.log('Created a new burger!');
+          location.reload();
+        });
+      });
+    }
+
+
+
+    // DELETE
+  const deleteBurgerBtns = document.querySelectorAll('.delete-burger');
+
+  // Set up the event listeners for each delete button
+  deleteBurgerBtns.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const id = e.target.getAttribute('data-id');
+
+      // Send the delete request
+      fetch(`/api/burgers/${id}`, {
+        method: 'DELETE',
+      }).then((res) => {
+        console.log(res);
+        console.log(`Deleted burger: ${id}`);
+
+        // Reload the page
+        location.reload();
+      });
+    });
+  });
+
+  });
+  
